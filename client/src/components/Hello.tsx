152438007711713c4433844628b9c69d702dcd11
Eleanor McMurtry <elmcmurtry1@gmail.com>
2020-06-03 13:21:22 +1000

    add eslint

--- client/src/components/Hello.tsx
+++ client/src/components/Hello.tsx
@@ -1,7 +1,7 @@
-import * as React from "react";
+import * as React from 'react';
 
-export interface HelloProps { compiler: string; framework: string; };
+export interface HelloProps { compiler: string; framework: string; }
 
-export const Hello = function (props: HelloProps) {
+export const Hello = function (props: HelloProps): React.ReactElement {
     return (<h1>Hello from {props.compiler} and {props.framework}</h1>);
 };
